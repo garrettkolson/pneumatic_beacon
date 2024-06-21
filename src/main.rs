a80edb76d5cd37e805f2a36f0636b2f7d1ce600a
garrettkolson <garrett@smallwerks.com>
2024-06-21 16:49:15 -0600

    pulled out server structs into core lib, moved beacon into lib.rs, removed beacon.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::net::TcpListener;
-use pneumatic_beacon::ThreadPool;
-use pneumatic_beacon::beacon::Beacon;
+use std::net::{TcpListener};
+use std::sync::Arc;
+use pneumatic_core::server;
+use pneumatic_beacon::Beacon;
 
 const THREAD_COUNT: usize = 100;
 
@@ -9,21 +10,21 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878")
         .expect("Couldn't set up listener for beacon");
 
-    let beacon = Beacon::from_config();
-    let pool = match ThreadPool::build(THREAD_COUNT) {
+    let beacon = Arc::new(Beacon::from_config());
+    let pool = match server::ThreadPool::build(THREAD_COUNT) {
         Err(err) => panic!("{}", err.message),
         Ok(p) => p
     };
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        let _ = pool.execute(|| {
-            beacon.handle_broadcast_request(stream);
-        });
+        let _ = match stream {
+            Ok(stream) => {
+                let safe_beacon = Arc::clone(&beacon);
+                let _ = pool.execute(move || {
+                    safe_beacon.handle_request(stream);
+                });
+            },
+            Err(_) => continue
+        };
     }
-
-    println!("Shutting down...")
 }
-
-
-
