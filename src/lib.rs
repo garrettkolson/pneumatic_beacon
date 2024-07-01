b51ab8c29ecdbccb547336b4e995f1344a195fd8
garrettkolson <garrett@smallwerks.com>
2024-07-01 14:57:33 -0600

    finished node registration implementations

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::thread;
 use std::thread::JoinHandle;
 use dashmap::DashMap;
 use pneumatic_core::{config, conns, encoding, node::*};
-use pneumatic_core::conns::{FireAndForgetSender, Sender};
+use pneumatic_core::conns::{Sender};
 use strum::IntoEnumIterator;
 
 pub struct Beacon {
@@ -31,7 +31,20 @@ impl Beacon {
         }
     }
 
-    /////////////// heartbeats /////////////////
+    ////////////// public methods //////////////
+
+    pub fn handle_request(&self, data: Vec<u8>) -> Vec<u8> {
+        let request = match encoding::deserialize_rmp_to::<NodeRequest>(data) {
+            Ok(req) => req,
+            Err(_) => return Self::get_heartbeat()
+        };
+
+        match request.request_type {
+            NodeRequestType::Register => self.register_node(request),
+            NodeRequestType::Request => self.request_nodes(request),
+            NodeRequestType::Heartbeat => Self::get_heartbeat()
+        }
+    }
 
     pub fn check_heartbeats(&self) -> HeartbeatResult {
         for node_type in NodeRegistryType::iter() {
@@ -42,6 +55,8 @@ impl Beacon {
 
         HeartbeatResult::Ok
     }
+
+    /////////////// heartbeats /////////////////
 
     fn check_nodes(&self, node_type: NodeRegistryType) -> HeartbeatResult {
         let nodes = match self.get_nodes(&node_type) {
@@ -84,23 +99,6 @@ impl Beacon {
         }
     }
 
-    fn push_registration_update(&self, batch: InternalRegistrationBatch) {
-        let data = Arc::new(RwLock::new(encoding::serialize_to_bytes_rmp(&batch)
-            .expect("Fatal serialization error pushing registration update")));
-
-        let addr = IpAddr::V6(Ipv6Addr::LOCALHOST);
-        let mut handles: Vec<JoinHandle<Vec<u8>>> = vec![];
-        for n_type in &self.config.node_registry_types {
-            let cloned_data = Arc::clone(&data);
-            let (conn, socket) = self.get_conn_and_addr(addr, conns::get_internal_port(n_type));
-            handles.push(conns::send_on_thread(cloned_data, conn, socket));
-        }
-
-        for handle in handles {
-            let _ = handle.join();
-        }
-    }
-
     fn broadcast_node_request(&self, node_type: &NodeRegistryType) {
         let request = self.get_broadcast_request(node_type);
         let data = Arc::new(RwLock::new(encoding::serialize_to_bytes_rmp(request)
@@ -120,11 +118,103 @@ impl Beacon {
             }
         }
 
-        // TODO: this needs to get the response back to add those nodes to the local list
+        for handle in handles {
+            let response_data = match handle.join() {
+                Ok(d) => d,
+                Err(_) => continue
+            };
+
+            let response = match encoding::deserialize_rmp_to::<NodeRegistryResponse>(response_data) {
+                Ok(r) => r,
+                Err(_) => continue
+            };
+
+            self.handle_registry_response(node_type, response.entries);
+        }
+    }
+
+    //////////// registration //////////////
+
+    fn register_node(&self, request: NodeRequest) -> Vec<u8> {
+        for node_type in request.requester_types {
+            if let Some(nodes) = self.get_nodes(&node_type) {
+                if !self.verify_node(&node_type, &request.requester_key) { continue; }
+                nodes.insert(request.requester_key.clone(), request.requester_ip.clone());
+                let reg = InternalRegistration::for_add(request.requester_key.clone(),
+                                                        request.requester_ip.clone(),
+                                                        vec![ node_type ]);
+                let batch = InternalRegistrationBatch::Add(vec![reg]);
+                self.push_registration_update(batch);
+            };
+        }
+
+        Self::get_heartbeat()
+    }
+
+    fn handle_registry_response(&self, node_type: &NodeRegistryType, entries: Vec<NodeRegistryEntry>) {
+        if let Some(nodes) = self.get_nodes(node_type) {
+            let mut new_nodes: Vec<InternalRegistration> = vec![];
+            for entry in entries {
+                if !self.verify_node(node_type, &entry.node_key) { continue; }
+                nodes.insert(entry.node_key.clone(), entry.node_ip.clone());
+                new_nodes.push(
+                    InternalRegistration::for_add(entry.node_key, entry.node_ip,
+                                                  vec![ node_type.clone() ]));
+            }
+
+            if new_nodes.len() == 0 { return; }
+            let batch = InternalRegistrationBatch::Add(new_nodes);
+            self.push_registration_update(batch);
+        }
+    }
+
+    fn verify_node(&self, node_type: &NodeRegistryType, key: &Vec<u8>) -> bool {
+        if self.node_is_already_registered(key, node_type) { return false; }
+        if self.type_is_maxed_out(node_type) { return false; }
+
+        // TODO: have to verify that this node has the proper minimum balance for this type
+
+        true
+    }
+
+    fn request_nodes(&self, request: NodeRequest) -> Vec<u8> {
+        let response = NodeRegistryResponse {
+            responder_key: self.config.public_key.clone(),
+            responder_ip: self.config.ip_address.clone(),
+            registry_type: request.requested_type.clone(),
+            entries: match self.get_nodes(&request.requested_type) {
+                None => vec![],
+                Some(nodes) => nodes.iter().map(|n| {
+                    NodeRegistryEntry {
+                        node_key: n.key().clone(),
+                        node_ip: n.value().clone()
+                    }
+                }).collect()
+            }
+        };
+
+        encoding::serialize_to_bytes_rmp(response)
+            .unwrap_or_else(|_| Self::get_heartbeat())
+    }
+
+    fn push_registration_update(&self, batch: InternalRegistrationBatch) {
+        let data = Arc::new(RwLock::new(encoding::serialize_to_bytes_rmp(&batch)
+            .expect("Fatal serialization error pushing registration update")));
+
+        let addr = IpAddr::V6(Ipv6Addr::LOCALHOST);
+        let mut handles: Vec<JoinHandle<Vec<u8>>> = vec![];
+        for n_type in &self.config.node_registry_types {
+            let cloned_data = Arc::clone(&data);
+            let (conn, socket) = self.get_conn_and_addr(addr, conns::get_internal_port(n_type));
+            handles.push(conns::send_on_thread(cloned_data, conn, socket));
+        }
+
         for handle in handles {
             let _ = handle.join();
         }
     }
+
+    ///////////////////// Common //////////////////////
 
     fn get_conn_and_addr(&self, node_addr: IpAddr, port: u16) -> (Box<dyn Sender>, SocketAddr) {
         let factory = Arc::clone(&self.conn_factory);
@@ -153,6 +243,13 @@ impl Beacon {
         }
     }
 
+    fn node_is_already_registered(&self, key: &Vec<u8>, node_type: &NodeRegistryType) -> bool {
+        match self.get_nodes(node_type) {
+            Some(nodes) => nodes.contains_key(key),
+            None => false
+        }
+    }
+
     fn get_min_node_number(&self, node_type: &NodeRegistryType) -> usize {
         match self.config.type_configs.get(node_type) {
             Some(node) => node.min,
@@ -160,61 +257,22 @@ impl Beacon {
         }
     }
 
-    //////////// external requests //////////////
-
-    pub fn handle_request(&self, data: Vec<u8>) -> Vec<u8> {
-        let request = match encoding::deserialize_rmp_to::<NodeRequest>(data) {
-            Ok(req) => req,
-            Err(_) => return Self::get_heartbeat()
-        };
-
-        match request.request_type {
-            NodeRequestType::Register => self.register_node(request),
-            NodeRequestType::Request => self.request_node(request),
-            NodeRequestType::Heartbeat => Self::get_heartbeat()
+    fn get_max_node_number(&self, node_type: &NodeRegistryType) -> usize {
+        match self.config.type_configs.get(node_type) {
+            Some(node) => node.max,
+            None => 0
         }
     }
 
-    fn register_node(&self, request: NodeRequest) -> Vec<u8> {
-        // TODO: have to make these checks more robust before allowing the registration
-        for node_type in request.requester_types {
-            if let Some(nodes) = self.get_nodes(&node_type) {
-                nodes.insert(request.requester_key.clone(), request.requester_ip.clone());
-                // TODO: write something to PUSH this registration to local processes
-            };
+    fn type_is_maxed_out(&self, node_type: &NodeRegistryType) -> bool {
+        match self.get_nodes(node_type) {
+            Some(nodes) => nodes.len() >= self.get_max_node_number(node_type),
+            None => true
         }
-
-        // TODO: fix this to return something meaningful
-        vec![]
-    }
-
-    fn request_node(&self, request: NodeRequest) -> Vec<u8> {
-        // TODO: need to redo this to just return serialized list of requested type
-        // TODO: log non-matched type requests
-        // let (nodes, port) = match request.requested_type {
-        //     NodeRegistryType::Committer => (Arc::clone(&self.committers), conns::COMMITTER_PORT),
-        //     NodeRegistryType::Sentinel => (Arc::clone(&self.sentinels), conns::SENTINEL_PORT),
-        //     NodeRegistryType::Executor => (Arc::clone(&self.executors), conns::EXECUTOR_PORT),
-        //     NodeRegistryType::Finalizer => (Arc::clone(&self.finalizers), conns::FINALIZER_PORT),
-        //     _ => return
-        // };
-        //
-        // // TODO: log serialization errors
-        // let data = match encoding::serialize_to_bytes_rmp(request) {
-        //     Ok(d) => d,
-        //     Err(_) => return
-        // };
-        //
-        // for node in nodes.iter() {
-        //     let addr = SocketAddr::new(node.value().clone(), port);
-        //     let sender = self.conn_factory.get_faf_sender();
-        //     sender.send(addr, &*data);
-        // }
-        todo!()
     }
 
     fn get_heartbeat() -> Vec<u8> {
-        vec![1u8]
+        vec![1u8, 2u8, 3u8, 4u8]
     }
 }
 
